40f58c4ca02090ded57fbd53b7bbeb31717c982f
Mayank Kesarwani <mayankkesarwani2778@gmail.com>
2020-03-30 22:07:41 +0530

    link added to whole component rather than just name

--- src/components/ConCard.js
+++ src/components/ConCard.js
@@ -6,53 +6,56 @@ function numberWithCommas(x) {
 export default function ConCard({ con, light }) {
   return (
     <div className="col-sm-12 col-md-4 col-lg-3 mb-4">
-      <div className={`card card-${light}`} style={{ height: "400px" }}>
-        <img
-          src={con.flag}
-          className="card-img-top"
-          alt="..."
-          style={{ width: "100%", height: "200px" }}
-        />
-        <div className="card-body">
-          <Link
-            className={`card-title text${light}`}
-            style={{ fontWeight: "bold", fontSize: "18px" }}
-            to={`/details/${con.alpha3Code}`}
-          >
-            {con.name}
-          </Link>
-          <p>
-            <span
-              className={`text${light}`}
-              style={{ fontWeight: "bold", fontSize: "16px" }}
-            >
-              Population :
-            </span>{" "}
-            <span className={`secondary-text`}>
-              {numberWithCommas(con.population)}
-            </span>
-          </p>
-          <p>
-            <span
-              className={`text${light}`}
-              style={{ fontWeight: "bold", fontSize: "16px" }}
-            >
-              Region :
-            </span>{" "}
-            <span className={`secondary-text`}>{con.region}</span>
-          </p>
+      <Link to={`/details/${con.alpha3Code}`}>
+        <div className={`card card-${light}`} style={{ height: "400px" }}>
+          <img
+            src={con.flag}
+            className="card-img-top"
+            alt="..."
+            style={{ width: "100%", height: "200px" }}
+          />
 
-          <p>
-            <span
-              className={`text${light}`}
-              style={{ fontWeight: "bold", fontSize: "16px" }}
+          <div className="card-body">
+            <Link
+              className={`card-title text${light}`}
+              style={{ fontWeight: "bold", fontSize: "18px" }}
+              to={`/details/${con.alpha3Code}`}
             >
-              Capital :
-            </span>{" "}
-            <span className={`secondary-text`}>{con.capital}</span>
-          </p>
+              {con.name}
+            </Link>
+            <p>
+              <span
+                className={`text${light}`}
+                style={{ fontWeight: "bold", fontSize: "16px" }}
+              >
+                Population :
+              </span>{" "}
+              <span className={`secondary-text`}>
+                {numberWithCommas(con.population)}
+              </span>
+            </p>
+            <p>
+              <span
+                className={`text${light}`}
+                style={{ fontWeight: "bold", fontSize: "16px" }}
+              >
+                Region :
+              </span>{" "}
+              <span className={`secondary-text`}>{con.region}</span>
+            </p>
+
+            <p>
+              <span
+                className={`text${light}`}
+                style={{ fontWeight: "bold", fontSize: "16px" }}
+              >
+                Capital :
+              </span>{" "}
+              <span className={`secondary-text`}>{con.capital}</span>
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
